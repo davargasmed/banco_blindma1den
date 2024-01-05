36bd126ac1f27b1080c2de8015598a56f9624d89
PALTLLESC <alejandro.201997@gmail.com>
2024-01-05 14:01:32 -0500

    realizamos un cambio en el apartado del menu donde el usuario seleccionaba la accion a realizar, se implemento inicialmente con sentencias if las cuales llamaban a las funciones segun la seleccion del usuario, sin embargo se decidio cambiar por la sentencia match case, el cual nos permite tener varias opciones sin la redundancia del if o elseif, todo esto lo encerramos dentro de un bucle while el cual permite hacer operaciones hasta que el usuario desee terminar

--- Banco.py
+++ Banco.py
@@ -8,8 +8,8 @@
 
 """
 
-banco = {'david22': ['miclave', 2000] ,
-         'liss97' : ['suclave' , 2000]
+banco = {'david22': ['miclave', 2000],
+         'liss97': ['suclave', 2000]
          }
 c = 3
 
@@ -19,7 +19,7 @@ def depositar(): #funcion para hacer depositos a la cuenta
     banco[user][1] = banco[user][1] + deposito
     print('su nuevo saldo es: $',banco[user][1])
 
-def retirar():#funcion para retirar dinero de la cuenta
+def retirar(): #funcion para retirar dinero de la cuenta
     retiro = int(input('Cuanto dinero quiere retirar?\n$'))
     if retiro > banco[user][1]:
         print('fondos insuficientes')
@@ -27,10 +27,10 @@ def retirar():#funcion para retirar dinero de la cuenta
         banco[user][1] = banco[user][1]-retiro
         print('su nuevo saldo es: ',banco[user][1])
 
-def ver_saldo():#funcion para ver el saldo en la cuenta
+def ver_saldo(): #funcion para ver el saldo en la cuenta
     print('su saldo es: ', banco[user][1])
 
-def transferir():#funcion para transferir dinero a otra cuenta
+def transferir(): #funcion para transferir dinero a otra cuenta
     print('a que cuenta quiere transferir dinero?')
 
 
@@ -46,24 +46,35 @@ for i in range(c): #con este bucle estaremos haciendo la validacion el cual usa 
             print ('***Ingreso correctamente***') #informa del ingreso correcto
             print('Su saldo es: ', banco[user][1])# muestra el saldo actual a modo de informacion
             print('*****************************************************************')
-            print('¿Qué deseas hacer?:\n' #este es nuestro menu de opciones luego de iniciar sesion 
-            '1.Depositar\n' # cada una de estas opciones esta representado por funciones las cuales estan definidas desde el principio del codigo
-            '2.Retirar\n'
-            '3.Ver saldo\n'
-            '4.Transferir\n'
-            '5.Nada')
 
-            opcion = int(input('Digita el numero de la opcion: ')) #pedimos al usuario ingresar el numero de la opcion que desea realizar
-            if opcion == 1:
-                depositar()
-            if opcion == 2:
-                retirar()
-            if opcion == 3:
-                ver_saldo()
-            if opcion == 4:
-                transferir()
-            if opcion == 5:
-                print('Gracias por usar nuestros servicios, hasta pronto.')
+
+            control = True
+            while control:
+
+                print('¿Qué deseas hacer?:\n'  # este es nuestro menu de opciones luego de iniciar sesion 
+                      '1.Depositar\n'  # cada una de estas opciones esta representado por funciones las cuales estan definidas desde el principio del codigo
+                      '2.Retirar\n'
+                      '3.Ver saldo\n'
+                      '4.Transferir\n'
+                      '5.Nada')
+                opcion = int(input(
+                    'Digita el numero de la opcion: '))  # pedimos al usuario ingresar el numero de la opcion que desea realizar
+
+                match opcion:
+                    case 1:
+                        depositar()
+
+                    case 2:
+                        retirar()
+                    case 3:
+                        ver_saldo()
+                    case 4:
+                        transferir()
+                    case 5:
+                        print('hasta luego')
+                        confirmacion = input('estas seguro?\n***********************************\n***escribe si o no***\n')
+                        if confirmacion == 'si':
+                            control = False
             break
 
         else:
@@ -74,5 +85,3 @@ for i in range(c): #con este bucle estaremos haciendo la validacion el cual usa 
         fallo = 2
         fallo = fallo-i
         print('usuario o contraseña incorrecto, intentos restantes:',fallo)
-
-#ponemos un comentario para probar
