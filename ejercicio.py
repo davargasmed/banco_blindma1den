e10a286e60730dfa0acb7336888b1b06bec4c8b0
David Vargas <alejandro.201997@gmail.com>
2024-01-19 18:57:25 -0500

    implementamos una sentencia try except para evitar que el usuario ingrese algo diferente a un un numero

--- ejercicio.py
+++ ejercicio.py
@@ -1,12 +1,18 @@
-horas =int(input('introduzca las horas:'))
-tarifa = int(input('introduzca la tarifa por hora:'))
-salario = 0
-if horas > 40:
-    extra = horas-40
-    ntarifa = tarifa*1.5
-    salario = (40*tarifa)+(extra*ntarifa)
-    print(f'su salario es {salario}')
-else:
-    salario = horas * tarifa
-    print(f'su salario es {salario}')
-    print('cambiod para nuevo comit')
+
+try:
+    horas =int(input('introduzca las horas:'))
+    tarifa = int(input('introduzca la tarifa por hora:'))
+    salario = 0
+
+    if horas > 40:
+        extra = horas-40
+        ntarifa = tarifa*1.5
+        salario = (40*tarifa)+(extra*ntarifa)
+        print(f'su salario es {salario}')
+    else:
+        salario = horas * tarifa
+        print(f'su salario es {salario}')
+
+except(ValueError):
+    print('por favor ingrese solo numeros')
+
